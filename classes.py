e21000eae51206ed00e6c7c9709da8c1527df548
unknown <tyrone.tong800@gmail.com>
2020-10-03 04:26:29 -0700

    part 1

--- classes.py
+++ classes.py
@@ -1,13 +1,10 @@
-
 global p
 
 p = []
 
 class Person:
     def __init__(self, name):
-
-        self.dict = 
-        {"housing": 0,
+        self.dict = {"housing": 0,
             "transportation":0,
             "food":0,
             "utilities":0,
@@ -28,18 +25,50 @@ class Person:
     def new_budget(self,budget):
         self.budget = budget
 
-def inpt():
-    n = input()
+    def add_finance(self,t,price):
+        self.dict[t] += price
+        self.budget -= price
+
+    def reset_dict(self):
+        self.dict = {"housing": 0,
+            "transportation":0,
+            "food":0,
+            "utilities":0,
+            "clothing":0,
+            "healthcare":0,
+            "insurance":0,
+            "supplies":0,
+            "personal":0,
+            "debt":0,
+            "retirement":0,
+            "education":0,
+            "savings":0,
+            "donations":0,
+            "entertainment ":0,
+        }
+    def reset_budget(self):
+        self.budget = 0
+
+    
+
+def inpt(m = ''):
+    n = input(m)
     return n
 
 def create_person():
-    name = inpt()
+    name = inpt("Name: ")
     x = Person(name)
     p.append(x)
 
 def new_bud(p):
-    b = inpt()
-    p.new_budget(b)
+    b = inpt("New Budget: ")
+    try:
+        b = int(b)
+        p.new_budget(b)
+        print("Success")
+    except:
+        print("Invalid")
+
 
 
 
--- main.py
+++ main.py
@@ -1,2 +1,55 @@
+import classes as c
+
 if __name__ == "__main__":
-    pass
+    c.create_person()
+    i = 7
+    lst = ["","Sunday", "Saturday", "Friday", "Thurday", "Wednesday", "Tuesday", "Monday"]
+    print(lst[i])
+    while True:
+        t = c.inpt("Enter Finance Category, 'ask', 'budget','next day',  or 'quit': ")
+
+
+        if (t.lower() in ["housing","transportation","food","utilities","clothing","healthcare","insurance","supplies","personal","debt","retirement","education","savings","donations","entertainment"]):
+            m = c.inpt("Enter Price: ")
+            try:
+                m = int(m)
+                c.p[0].add_finance(t,m)
+                if c.p[0].budget < 0:
+                    print("You spent way too much !")
+            except:
+                print("Invalid")
+
+
+        elif t.lower() == "budget":
+            c.new_bud(c.p[0])
+
+
+        elif t.lower() == "ask":
+            m = c.inpt("Price: ")               #need help with algorithm
+            try:
+                m = float(m)
+                p = float(1/float(i))
+                if (m/float(c.p[0].budget)) < p:
+                    print("Go For It")
+                else:
+                    print("Nah")
+            except:
+                print("Invalid")
+
+
+        elif t.lower() == "next day":
+            i-=1
+            if i == 0:
+                i = 7
+            print(lst[i])
+    
+
+
+        elif t.lower() == "quit":
+            break
+
+
+        else:
+            pass
+
+        
