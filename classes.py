694c70337c733d5792d64d2d6a57662d92845985
unknown <tyrone.tong800@gmail.com>
2020-10-02 19:53:45 -0700

    f

---
+++ classes.py
@@ -0,0 +1,3 @@
+def in():
+    n = input()
+    return n
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from flask import Flask, url_for, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('home.html')
+
+
+
 if __name__ == "__main__":
-    pass
+    app.run()
