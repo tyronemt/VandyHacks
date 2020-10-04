4ac3db8dd029d198e9566522758563ead498bc86
Joshua Zhang <joshzhang123@gmail.com>
2020-10-03 19:24:41 -0700

    uploading graph

--- graph.py
+++ graph.py
@@ -3,22 +3,21 @@ import matplotlib.pyplot as plt
 class Graph:
     def __init__(self, person):
         self.person = person
+        dic = self.person.dict
+        self.labels = []
+        self.totalSpent = []
+        for label, spent in dic.items():
+            if spent != 0:
+                self.labels.append(label)
+                self.totalSpent.append(spent)
 
     def display(self):
-        dic = self.person.dict
-        labels = []
-        sizes = []
         circle = plt.Circle((0, 0), 0.7, color="white")
-        for label, size in dic.items():
-            if size != 0:
-                labels.append(label)
-                sizes.append(size)
-        plt.pie(sizes, labels=labels, autopct="%1.1f%%")
+        plt.pie(self.totalSpent, labels=self.labels, autopct="%1.1f%%")
         plt.axis("equal")
         p = plt.gcf()
         p.gca().add_artist(circle)
         plt.show()
-
 def create_graph(person):
     return Graph(person)
 
